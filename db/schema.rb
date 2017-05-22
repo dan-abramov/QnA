12dcc54492c7804ba58594cd0cf5de66f4150047
dan-abramov <mymindsgate@gmail.com>
2017-05-22 14:00:48 +0300

    added foregin key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520224005) do
+ActiveRecord::Schema.define(version: 20170522105208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,4 +29,5 @@ ActiveRecord::Schema.define(version: 20170520224005) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
