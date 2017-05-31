8c684796369a391b44ac1cfbf38827a2fb2181cd
dan-abramov <mymindsgate@gmail.com>
2017-05-31 15:21:20 +0300

    added acceptance test: sign_out.rb

---
+++ spec/acceptance/sign_out_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'User sign out', '
+  As user
+  I want to end my session
+' do
+
+  given(:user) { create(:user) }
+
+  scenario 'User click button "Sign out"' do
+    sign_in(user)
+    visit root_path
+    click_on 'Sign out'
+
+    expect(page).to have_content 'Signed out successfully.'
+    expect(current_path).to eq root_path
+  end
+end
