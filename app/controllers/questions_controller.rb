1344d4ad790dd85a324f1584ee7e80e818f21a41
dan-abramov <mymindsgate@gmail.com>
2017-06-08 22:35:28 +0300

    all tests passed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -31,7 +31,7 @@ class QuestionsController < ApplicationController
 
   def update
     if @question.update(question_params)
-      redirect_to questions_path(@question)
+      redirect_to question_path(@question)
     else
       render :edit
     end
