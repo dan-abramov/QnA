c181f3784ac463fd3a4c017bb6831ddc12a78ae5
dan-abramov <mymindsgate@gmail.com>
2017-06-24 22:51:05 +0300

    added polymorphic attachments and answer assosiation with attachments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,7 +3,10 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
+  has_many :attachments, as: :attachable
   validates :body, presence: true
+
+  accepts_nested_attributes_for :attachments
 
   default_scope { order(best: :desc) }
 
