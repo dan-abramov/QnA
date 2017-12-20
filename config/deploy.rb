3bbde3c386ef21eb34aafa8306057410caf3639e
Unknown <mymindsgate@gmail.com>
2017-12-20 14:50:18 +0300

    Auto stash before merge of "master" and "origin/master"

---
+++ config/deploy.rb
@@ -0,0 +1,50 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.10.1"
+
+set :application, "TN-A"
+set :repo_url, "git@github.com:dan-abramov/thinknetica-advanced.git"
+
+set :deploy_to, "/home/deployer2/TH-A"
+set :deploy_user, "deployer2"
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", "config/secrets.yml", ".env"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+ 
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp.restart.txt')
+
+    end
+  end
+
+  after :publishing, :restart
+end
