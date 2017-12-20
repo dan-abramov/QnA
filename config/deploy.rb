c45262cf4e7d7f2bb1542d83f0db538cd96eab72
Unknown <mymindsgate@gmail.com>
2017-12-21 00:30:52 +0300

    add Capfile and deploy folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,11 +11,11 @@ set :deploy_user, "deployer2"
 append :linked_files, "config/database.yml", "config/secrets.yml", ".env"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
- 
+
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
